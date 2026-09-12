826b9e6de0bc76ea5bf9b5fec8f82a0afd408042
kri <cristobalame@gmail.com>
2026-09-12 16:03:49 -0600

    feat: add financial education UI

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EducationalTopicController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/education', [EducationalTopicController::class, 'index'])
+    ->name('education.index');
+
+Route::get('/education/{educationalTopic}', [EducationalTopicController::class, 'show'])
+    ->name('education.show');
