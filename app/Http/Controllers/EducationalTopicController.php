6b54b762a8392659e2ea2333e5dccec00de56032
kri <cristobalame@gmail.com>
2026-09-12 18:01:49 -0600

    Cambios

--- app/Http/Controllers/EducationalTopicController.php
+++ app/Http/Controllers/EducationalTopicController.php
@@ -4,18 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\EducationalTopic;
 use App\Services\FinancialEducationService;
-
+use App\Services\FinancialEducationIntegrationService;
 
 class EducationalTopicController extends Controller
 {
-    public function index(FinancialEducationService $service)
-    {
+    public function index(
+        FinancialEducationService $service,
+        FinancialEducationIntegrationService $integrationService
+    ) {
         $user = \App\Models\User::first();
 
         $learningPath = $service->getLearningPath($user);
 
+        $financialContext = $integrationService
+            ->getFinancialContext($user);
+
         return view('education.index', [
             'learningPath' => $learningPath,
+            'financialContext' => $financialContext,
         ]);
     }
 
